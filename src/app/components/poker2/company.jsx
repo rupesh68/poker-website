84f5223aacfae46da695c7562d575cff1fdf734f
Rupesh Mahajan <rupeshmahajan516@gmail.com>
2024-07-13 18:56:32 +0530

    change done

--- src/app/components/poker2/company.jsx
+++ src/app/components/poker2/company.jsx
@@ -17,17 +17,15 @@ export default function Company() {
       <div className="max-w-screen-xl px-4 pb-8 mx-auto lg:pb-16">
         <div className="grid grid-cols-2 gap-8 text-white sm:gap-12 sm:grid-cols-3 lg:grid-cols-4 ">
           {sponser.map((img, index) => (
-            <>
-              <Link
-                key={index}
-                href=""
-                className="flex items-center lg:justify-center"
-              >
-                <picture>
-                  <img src={img} alt="img" />
-                </picture>
-              </Link>
-            </>
+            <Link
+              key={index}
+              href=""
+              className="flex items-center lg:justify-center"
+            >
+              <picture>
+                <img src={img} alt="img" />
+              </picture>
+            </Link>
           ))}
         </div>
       </div>
--- src/app/components/poker2/temp.jsx
+++ src/app/components/poker2/temp.jsx
@@ -3,22 +3,14 @@ import { useEffect } from "react";
 export default function Temp({ mediaSrc, cls }) {
   const isVideo = mediaSrc.includes(".mp4");
   useEffect(() => {
-    console.log(isVideo, mediaSrc)
+    // console.log(isVideo, mediaSrc)
   }, []);
   return (
     <>
-      {isVideo && <video src={mediaSrc} className={cls}
-        autoPlay
-        loop
-        muted/>
-        
-        
-        
-        
-        }
+      {isVideo && <video src={mediaSrc} className={cls} autoPlay loop muted />}
       {!isVideo && (
         <picture>
-          <img src={mediaSrc} alt="img" className={cls}/>
+          <img src={mediaSrc} alt="img" className={cls} />
         </picture>
       )}
     </>
